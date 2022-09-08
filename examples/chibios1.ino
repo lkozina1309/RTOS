69022a96d5f52f494bdf3932a45e24c33c61075f
Luka Kozina <lukakozina6@gmail.com>
2022-09-08 14:21:18 +0200

    Update chibios1.ino

--- examples/chibios1.ino
+++ examples/chibios1.ino
@@ -78,12 +78,3 @@ void chSetup() {
 //------------------------------------------------------------------------------
 void loop() {/* not used */}
 
-void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
